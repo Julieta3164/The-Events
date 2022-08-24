c2da7d76d06ca27e41a009747077a1732b8d1758
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-08-24 12:13:44 +0200

    upload

--- database/migrations/2022_08_23_213347_create_events_table.php
+++ database/migrations/2022_08_23_213347_create_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
-             $table->unsignedBigInteger('event_id');  
+           $table->unsignedBigInteger('user_id');  
             $table->string('name');
 
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('img');
             $table->timestamps();
 
-            $table->foreign('event_id')->references('id')->on('events');  
+         $table->foreign('user_id')->references('id')->on('users'); 
         });
     }
 
@@ -35,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('events');
     }
 }
