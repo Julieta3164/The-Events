ef7a66de97c55278f8fb3446851b942a486317b8
Paz Manrique <nautilus1985@gmail.com>
2022-08-25 14:16:49 +0200

    I made a test by myself

--- public/js/app.js
+++ public/js/app.js
@@ -22287,12 +22287,9 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/**
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
   \*********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\nError: Can't resolve '../assets/fondo/ hf.png' in 'C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\resources\\sass'\n    at finishWithoutResolve (C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\enhanced-resolve\\lib\\Resolver.js:309:18)\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\enhanced-resolve\\lib\\Resolver.js:386:15\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\enhanced-resolve\\lib\\Resolver.js:435:5\n    at eval (eval at create (C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:16:1)\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\enhanced-resolve\\lib\\Resolver.js:435:5\n    at eval (eval at create (C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:27:1)\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:87:43\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\enhanced-resolve\\lib\\Resolver.js:435:5\n    at eval (eval at create (C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\enhanced-resolve\\lib\\Resolver.js:435:5\n    at processResult (C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\webpack\\lib\\NormalModule.js:758:19)\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\webpack\\lib\\NormalModule.js:860:5\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\loader-runner\\lib\\LoaderRunner.js:400:11\n    at C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\loader-runner\\lib\\LoaderRunner.js:252:18\n    at context.callback (C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at Object.loader (C:\\Users\\Usuario\\Documents\\Bootcamp\\Laravel\\The-Events\\node_modules\\css-loader\\dist\\index.js:155:5)\n    at processTicksAndRejections (node:internal/process/task_queues:96:5)");
 
 /***/ }),
 
