d88837215e589d6cd40853c431f7ab2128704955
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-26 16:26:50 +0200

    problems

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -3,21 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-<<<<<<< HEAD
-        <link rel="stylesheet" type="text/css" href="{{asseet('css/estilo.css')}}">
-=======
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
->>>>>>> develop
         <title>Laravel</title>
 
     </head>
 
 
-<<<<<<< HEAD
-    
-    <body class="antialiased">
-        <div class="buttons">
-=======
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
@@ -26,31 +17,19 @@
     </head>
     <body >
         
->>>>>>> marta3
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Panel Personal</a>
                     @else
-<<<<<<< HEAD
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Iniciar Sección</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registro</a>
-=======
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Inicia Sesión</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Regístrate</a>
->>>>>>> develop
                         @endif
                     @endauth
                 </div>
             @endif
-<<<<<<< HEAD
-        </div>
-        
-=======
 
 
 
@@ -62,6 +41,5 @@
                 
           
 
->>>>>>> develop
     </body>
 </html>
