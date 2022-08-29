a75c25f3841f2c99e819ccfbde0cc5f6169de919
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-29 11:26:01 +0200

    check header basic

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,32 +9,18 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>header</title>
 </head>
+
+
+
+
 <body>
-   <header>
-    <div class="header">
-        @if (Route::has('login'))
-            
-              <div class="logo">
-                <img id="constructionImg" src="{{ asset('./img/logo.png') }}" />
-              </div>
-              <div class="btn">
-                @auth
-                    <a href="{{ url('/dashboard') }}" class="">Panel Personal</a>
-                @else
-                  <button  type="button" class="btn btn-danger btn-sm me-md-2">
-                    <a href="{{ route('login') }}" class="btn btn-danger btn-sm me-md-2">Inicia Sesión</a>
-                  </button>
-
-                    @if (Route::has('register'))
-                       <button type="button"  class="btn btn-danger btn-sm">
-                        <a href="{{ route('register') }}" class="btn btn-danger btn-sm">Regístrate</a>
-                       </button>
-                    @endif
-                @endauth
-              </div>
-            </div>
-        @endif
-    </header>
-
+  <header class="header">
+    <div class="cabecera">
+    <img id="img-header" src="{{ asset('./img/header.png') }}" />
+  </div>
+      <div class="logo">
+        <img id="constructionImg" src="{{ asset('./img/logo.png') }}" />
+      </div>
+  </header>
 </body>
 </html>                     
