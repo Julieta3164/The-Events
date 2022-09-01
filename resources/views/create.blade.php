de91a3cb3e0beaa6475e1f5ebdbf8f2100724c7a
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-09-01 15:21:46 +0200

    view events init

--- resources/views/create.blade.php
+++
@@ -1,4 +0,0 @@
-<x-constructionpage/>
-<footer>
-    <h5>Esta es la create page</h5>
-</footer>
--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -1,35 +1,43 @@
-{{-- <x-app-layout>
-    <x-header/>
-    @extends('layouts.baseevents') --}}
+<x-header/>
 
-<a href="events/create" class="btn btn-primary">CREAR</a>
 
-<table class="table table-dark table-striped mt-4">
-    <thead>
-      <tr>
-        <th scope="col">Título</th>
-        <th scope="col">Descripción</th>
-        <th scope="col">Fecha</th>
-        <th scope="col">Hora</th>
-        <th scope="col">Personas</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($event as $event)
-          <td>{{$event->title}}</td>
-          <td>{{$event->description}}</td>
-          <td>{{$event->date}}</td>
-          <td>{{$event->time}}</td>
-          <td>{{$event->people}}</td>
-          <td>
-            <form action="{{ route('events.destroy',$event->id) }}" method="POST">
-             <a href="/events/{{$event->id}}/edit" class="btn btn-info">Editar</a>         
-                 @csrf
-                 @method('DELETE')
-             <button type="submit" class="btn btn-danger">Delete</button>
-            </form>          
-           </td>        
-       </tr>
-      @endforeach    
+
+
+
+  <table class="table  table-striped mt-8 table-event">
+      @foreach ($event as $event)   
+      <div class="img-card table-event">
+        <button type="submit" class="btn-delete"><i class="bi bi-trash"></i></button>
+      </div>
+
+      <div>
+        <img src="{{ asset('./img/cons.jpg') }}" class="card-img-top" alt="...">
+      </div>
+
+      <div>
+        <form   action="{{ route('events.destroy',$event->id) }}" method="POST">
+          <a class="btn-edit" href="/events/{{$event->id}}/edit" >Editar</a>         
+             @csrf
+             @method('DELETE')
+        </form> 
+      </div>
+      
+                    
+      @endforeach   
     </tbody>
   </table>
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+ 
