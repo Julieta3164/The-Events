477b4eb1060b7a00816d422b76add922e7937f6b
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-09-02 11:47:45 +0200

    intentando

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -1,25 +1,24 @@
 <x-header/>
+<x-buttonevents/>
 
-  <table class="table  table-striped mt-8 table-event">
-      @foreach ($event as $event)   
-      <div class="img-card table-event">
-        <button type="submit" class="btn-delete"><i class="bi bi-trash"></i></button>
-      </div>
+  <table class="table  table-striped mt-8 table-event" >
+      @foreach ($event as $event)
+      <div class="img-card ">
+        <button type="submit" class="btn-delete"> borrar{{-- <i class="bi bi-trash"></i> --}}</button>
+    
 
-      <div>
+    
         <img src="{{ asset('./img/cons.jpg') }}" class="card-img-top" alt="...">
-      </div>
 
-      <div>
         <form   action="{{ route('events.destroy',$event->id) }}" method="POST">
           <a class="btn-edit" href="/events/{{$event->id}}/edit" >Editar</a>         
             @csrf
             @method('DELETE')
         </form> 
-      </div>
+  
       
                     
-      @endforeach   
+      @endforeach
     </tbody>
   </table>
 
