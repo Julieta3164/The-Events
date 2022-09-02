414dcfb040dbcb01643603f9393b900e02614ebb
Juliet Linarez <julietlinarez@gmail.com>
2022-09-02 19:07:45 +0200

    fix routes and file upload

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -2,22 +2,23 @@
 <x-buttonevents/>
 
   <table class="table  table-striped mt-8 table-event" >
-      @foreach ($event as $event)
+      @foreach ($event as $events)
       <div class="img-card ">
-        <button type="submit" class="btn-delete"> borrar{{-- <i class="bi bi-trash"></i> --}}</button>
     
 
     
         <img src="{{ asset('./img/cons.jpg') }}" class="card-img-top" alt="...">
 
-        <form   action="{{ route('events.destroy',$event->id) }}" method="POST">
-          <a class="btn-edit" href="/events/{{$event->id}}/edit" >Editar</a>         
+
+        <form   action="{{ route('events.destroy',['id'=>$events->id]) }}" method="POST">
+          <button type="submit" class="btn-delete"> borrar{{-- <i class="bi bi-trash"></i> --}}</button>
+
+          <a class="btn-edit" href="/events/{{$events->id}}/edit" >Editar</a>         
             @csrf
             @method('DELETE')
         </form> 
-  
-      
-                    
+
+
       @endforeach
     </tbody>
   </table>
--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,17 @@ use Illuminate\Support\Facades\Route;
 
     
 Route::group(['middleware' => ['auth', 'admin']], function () {
-    Route::resource('/events', EventController::class);
-    Route::resource('/events/create', CreateController::class);
-    Route::resource('/events/edit', EditController::class);
-});
+    Route::get('/events', [EventController::class, 'index' ]);
+    Route::post('/events', [EventController::class, 'save' ])->name('events.save');
 
+    Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
+    Route::delete('/events/destroy', [EventController::class, 'destroy'])->name('events.destroy');
+    
+    Route::get('/events/{id}/edit', [EventController::class, 'edit'])->name("events.edit");
+    Route::put('/events/{id}/edit', [EventController::class, 'update'])->name("events.update");
+
+
+});
         
 /* Route::resource('dashboard', DashboardController::class)->middleware(['auth']);  */ 
 Route::get('/dashboard', function () {
