e42984af8374729516effe10106a1fb905a4ac5d
Juliet Linarez <julietlinarez@gmail.com>
2022-09-05 13:09:46 +0200

    merge

--- resources/views/components/buttonevents.blade.php
+++ resources/views/components/buttonevents.blade.php
@@ -1,7 +1,3 @@
 <div class="button-cards">
-<<<<<<< HEAD
     <a href="{{ url('/events/create') }}" class="btn btn-primary btn-primary1">Crear</a>
-=======
-    <a href="{{ route('events.save') }}" class="btn btn-primary btn-primary1">Crear</a>
->>>>>>> mod_routes
-    </div>
+</div>
