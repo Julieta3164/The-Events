52cb384c2ac1378e2b35f479a89bddb1067af4c4
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-09-07 12:32:42 +0200

    contenedor de cartas made in marta

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -1,36 +1,23 @@
-<div>
-    @foreach ($event as $events)
-    <div class="card card-home" style="width: 18rem;">
-        <div class="img-card">
-            <img src="{{$events->image}}" class="card-img-top" alt="...">
-        </div>
+<div class="contenedor-cartas">
+ @foreach ($event as $events)
+  <div class="card card-home" style="width: 18rem;">
 
-        <div class="card-body">
-            <div class="card-title2">
-        <h5  class="card-title ">Titulo: {{$events->title}}
-{{--             @foreach ($events as $event)
-                {{ $event->title}}
-            @endforeach --}}
-    </h5>
-
-        <p class="card-text"> Fecha: {{$events->date}}
-{{--             @foreach ($events as $event)
-                {{ $event->Date}}
-            @endforeach --}}
-        </p>
-
-        <p class="card-text">Hora: {{$events->time}}
-{{--             @foreach ($events as $event)
-                {{ $event->Hora}}
-            @endforeach --}}
-        </p>
-        
+    <div class="img-card">
+       <img src="{{$events->image}}" class="card-img-top" alt="...">
     </div>
-        <div class="button-cards">
+
+    <div class="card-body">
+      <div class="card-title2">
+        <h5  class="card-title ">Titulo: {{$events->title}}</h5>
+        <p class="card-text"> Fecha: {{$events->date}}</p>
+        <p class="card-text">Hora: {{$events->time}}</p> 
+      </div>
+
+      <div class="button-cards">
         <a href="{{ url('/description') }}" class="btn btn-primary btn-primary1">Mas Información</a>
         <a href="#" class="btn btn-primary btn-primary2">Solicitar Plaza</a>
+      </div>
     </div>
-        </div>
-    </div>
+  </div>
     @endforeach
 </div>
